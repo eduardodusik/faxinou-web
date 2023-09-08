0e294517b890ff598c525eea8840a922a79be5bd
eduardo.dusik <eduardo.dusik@ifood.com.br>
2023-09-08 15:57:08 -0300

    feat: add meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import SupabaseProvider from "@/app/supabase-provider";
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Faxinou',
-  description: 'A melhor plataforma de faxina do Brasil',
-}
 
 export const dynamic = 'force-dynamic'
 
@@ -20,6 +14,11 @@ export default function RootLayout({
 
   return (
     <html lang="pt-BR">
+    <head>
+      <title>Faxinou</title>
+      <meta name="description" content="A melhor plataforma de faxina do Brasil" />
+      <meta name="viewport" content="width=device-width, user-scalable=no" />
+    </head>
       <body className={inter.className}>
         <SupabaseProvider>
           {children}
